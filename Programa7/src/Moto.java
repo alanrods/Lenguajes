1833a4226dbf7aa23cd0d8394342cb2ba0dad3ff
AlexPlata <alejandro.plata.mtz@hotmail.com>
2016-05-05 23:15:56 -0500

    Update Moto.java

--- Programa7/src/Moto.java
+++ Programa7/src/Moto.java
@@ -1,11 +1,21 @@
-
+/**
+ * Permite crear instancias de tipo moto
+ * 
+ */
 public class Moto extends Vehiculo {
 
     private int motoSpeed = 150;
-
+/**
+ * Constructor
+ * @param No recibe parámetros
+ */
     public Moto(){
     }
-
+    
+/**
+ * Getter
+ * @return Velocidad de la moto:motoSpeed
+ */
     public int f(){
         return motoSpeed;
     }

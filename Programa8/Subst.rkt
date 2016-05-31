8439cab8a33eb2753707c3f7a4dbdbd2704eda9f
alanrods <alanrods@outlook.com>
2016-05-30 23:33:13 -0500

    Nombres
    :dancer:

--- Programa8/Subst.rkt
+++ Programa8/Subst.rkt
@@ -1,6 +1,13 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname Subst) (read-case-sensitive #t) (teachpacks ((lib "show-queen.ss" "teachpack" "htdp") (lib "image.ss" "teachpack" "2htdp") (lib "universe.ss" "teachpack" "2htdp") (lib "batch-io.ss" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "show-queen.ss" "teachpack" "htdp") (lib "image.ss" "teachpack" "2htdp") (lib "universe.ss" "teachpack" "2htdp") (lib "batch-io.ss" "teachpack" "2htdp")) #f)))
+#|
+Moreno Tagle Raphael Iván
+Plata Martínez Jesús Alejandro
+Rodríguez Bribiesca Isaac
+Rodríguez García Alan Julian
+|#
+
 #|A. Definición de datos para la representación de expresiones 
 |#
 

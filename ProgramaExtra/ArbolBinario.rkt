5610aeace26bcaee4662f2584fbd3835db69be27
IsaacRodgz <chack.rb@gmail.com>
2016-05-30 23:38:40 -0500

    Árbol Binario

--- ProgramaExtra/ArbolBinario.rkt
+++ ProgramaExtra/ArbolBinario.rkt
@@ -3,8 +3,32 @@
 #reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname ArbolBinario) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (define-struct nodo (valor izq der))
 
-(define (numhojas ab)
+(define (crea-nodo valor) (make-nodo valor null null))
+
+(define (inserta-nodo nodo-n ab)
   (cond
-    [(number? ab) 1]
-    [(nodo? ab)(+(numhojas(nodo-izq ab))
-                (numhojas(nodo-der ab)))]))
+    [(< (nodo-valor nodo-n)(nodo-valor ab)) (cond
+                                              [(null? (nodo-izq ab)) nodo-n]
+                                              [else
+                                               (make-nodo
+                                                (nodo-valor ab) (inserta-nodo nodo-n (nodo-izq ab)) (inserta-nodo nodo-n (nodo-izq ab)))])]
+    [(> (nodo-valor nodo-n)(nodo-valor ab)) (cond
+                                              [(null? (nodo-der ab)) nodo-n]
+                                              [else
+                                               (make-nodo
+                                                (nodo-valor ab) (inserta-nodo nodo-n (nodo-izq ab)) (inserta-nodo nodo-n (nodo-izq ab)))])]))
+
+(define (num-hojas ab)
+  (cond
+    [(and (null? (nodo-izq ab)) (null? (nodo-der ab))) 1]
+    [(nodo? ab)(+(num-hojas(nodo-izq ab))
+                (num-hojas(nodo-der ab)))]))
+
+
+#|
+(struct posn (x y)#:transparent)
+(struct-copy my-struct s [f2 200])
+;;=> (my-struct 1 200 3 4)
+(struct-copy my-struct s [f2 (* 100 (my-struct-f2 s))])
+;;=> (my-struct 1 200 3 4)
+|#
